0464e396cdc1b44628f79c747882dfa94b94459a
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-24 13:56:22 +0000

    Session 1: Challenges 1-7 complete

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,27 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  arr = string.split ""
+  if return_odds == true
+   arr.select.with_index {|_, x| x.odd?}.join('')
+ else
+   arr.select.with_index {|_, x| x.even?}.join('')
+ end
 end
+
+
+
+ #new_string = ""
+ #string.length.times do |position|
+ #  next if return_odds && position.even?
+ #  next if !return_odds && position.odd?
+ #    new_string << string[position]
+ #  end
+ #  new_string
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,26 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_string = "" #creates a new empty string
+
+  string.size.times do |index| #takes the length of the string, e.g X. does following X times. Adds Index as argument for block.
+    current_char = string[index] #sets current_char as string index
+    next_char = string[index +1] || '' #sets next_char as string index incremented by 1.
+    if current_char == "r" || current_char == "R" #checks for lowercase and uppercase r.
+      new_string << next_char #adds next_char into new_string
+    end
+  end
+  new_string #calls new string
+
+
 end
+
+#arr = string.split ""
+#index = arr.index("r")
+#if arr.include? "r"
+#  return index[+1]
+#end
